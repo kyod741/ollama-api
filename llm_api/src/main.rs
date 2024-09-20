7bd685fd33960eb5784baee5e37d00bf6748901c
kyod741 <kyod741@staszic.waw.pl>
2024-09-20 19:52:13 +0200

    Added a simple cli tool for generating new jwt tokens

--- llm_api/src/main.rs
+++ llm_api/src/main.rs
@@ -1,11 +1,30 @@
+use std::env::args;
 mod utils;
 
+const INFO: &str = "
+Usage
+    help:
+        prints help
+    new:
+        creates a new token and prints it
+";
+
+fn help() -> (){
+    println!("{INFO}");
+}
+
+fn new_token() -> String{
+    utils::generate_token().unwrap()
+}
 fn main(){
-    let token: String = utils::generate_token().unwrap();
-    println!("{}", token);
-    let result = utils::validate_token(&token);
-    match result{
-        Ok(_) => println!("git"),
-        Err(e) => println!("{e}"),
-    };
+    let args: Vec<String> = args().collect();
+    if args.len() >= 2 && args[1] == "new" {
+        let token: String = new_token();
+        println!("{token}");
+    }else if (args.len() >=2 && args[1] == "help") || args.len() == 1 {
+        help();
+    }else{
+        println!("could not recognize any command");
+        help();
+    }
 }
