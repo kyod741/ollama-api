0a34f0c1b1376b9af6c371f19fa33b64d0269e21
kyod741 <kyod741@staszic.waw.pl>
2024-09-29 23:51:57 +0200

    Now the traits FromData and FromRequest are implemented for ChatCompletionRequest also, it was a lot of work tho

--- src/user/routers.rs
+++ src/user/routers.rs
@@ -1,7 +1,11 @@
-use crate::user::schemas::{CompletionRequestBody, CompletionRequestHeaders};
+use crate::user::schemas::{CompletionRequestBody, CompletionRequestHeaders, Wrapper};
 
 #[get("/", data = "<request>")]
-pub fn completion_request(token: CompletionRequestHeaders, request: CompletionRequestBody) {
+pub fn completion_request(
+    token: Wrapper<CompletionRequestHeaders>,
+    request: Wrapper<CompletionRequestBody>,
+) {
+    let unwrapped_token = token.0;
     println!("{request:#?}");
-    println!("{token:#?}");
+    println!("{unwrapped_token:#?}");
 }
--- src/user/schemas.rs
+++ src/user/schemas.rs
@@ -1,5 +1,6 @@
 use crate::user::utils::validate_token;
 use ::serde::{Deserialize, Serialize};
+use derive_more::From;
 use rocket::data::{self, FromData, ToByteUnit};
 use rocket::outcome::Outcome;
 use rocket::request::{self, FromRequest};
@@ -25,18 +26,38 @@ pub enum PayloadError {
     Io(std::io::Error),
 }
 
-#[derive(Debug)]
-pub struct CompletionRequestBody {
-    pub payload: String,
-}
+pub trait IsCompletionRequestHeaders {}
+pub trait IsCompletionRequestBody {}
 
-#[derive(Debug)]
+#[derive(Debug, From)]
+pub struct CompletionRequestBody(String);
+
+#[derive(Debug, From)]
 pub struct CompletionRequestHeaders {
     pub token: String,
 }
 
+#[derive(Debug, From)]
+pub struct ChatCompletionRequestBody(String);
+
+#[derive(Debug, From)]
+pub struct ChatCompletionRequestHeaders {
+    pub token: String,
+}
+
+impl IsCompletionRequestHeaders for CompletionRequestHeaders {}
+impl IsCompletionRequestHeaders for ChatCompletionRequestHeaders {}
+impl IsCompletionRequestBody for CompletionRequestBody {}
+impl IsCompletionRequestBody for ChatCompletionRequestBody {}
+
+#[derive(Debug)]
+pub struct Wrapper<T>(pub T);
+
 #[rocket::async_trait]
-impl<'r> FromRequest<'r> for CompletionRequestHeaders {
+impl<'r, T> FromRequest<'r> for Wrapper<T>
+where
+    T: IsCompletionRequestHeaders + From<String>,
+{
     type Error = JWTTokenError;
     async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
         let token_option = request.headers().get_one("Authorization");
@@ -44,9 +65,7 @@ impl<'r> FromRequest<'r> for CompletionRequestHeaders {
             if token.len() > 6 && &token[0..7] == "Bearer " {
                 let extracted_token = &token[7..];
                 match validate_token(extracted_token) {
-                    Ok(_) => Outcome::Success(CompletionRequestHeaders {
-                        token: extracted_token.to_string(),
-                    }),
+                    Ok(_) => Outcome::Success(Wrapper(T::from(extracted_token.to_string()))),
                     Err(_) => Outcome::Error((Status::Unauthorized, JWTTokenError::Invalid)),
                 }
             } else {
@@ -59,7 +78,10 @@ impl<'r> FromRequest<'r> for CompletionRequestHeaders {
 }
 
 #[rocket::async_trait]
-impl<'r> FromData<'r> for CompletionRequestBody {
+impl<'r, T> FromData<'r> for Wrapper<T>
+where
+    T: IsCompletionRequestBody + From<String>,
+{
     type Error = PayloadError;
     async fn from_data(_request: &'r Request<'_>, data: Data<'r>) -> data::Outcome<'r, Self> {
         use PayloadError::*;
@@ -70,13 +92,11 @@ impl<'r> FromData<'r> for CompletionRequestBody {
             .expect("STRING_SIZE_LIMIT_IN_BYTES must be a valid integer");
 
         match data.open(size_limit.bytes()).into_string().await {
-            Ok(string) if string.is_complete() => Outcome::Success(CompletionRequestBody {
-                payload: string.into_inner(),
-            }),
+            Ok(string) if string.is_complete() => {
+                Outcome::Success(Wrapper(T::from(string.into_inner())))
+            }
             Ok(_) => Outcome::Error((Status::PayloadTooLarge, TooLarge)),
             Err(e) => Outcome::Error((Status::InternalServerError, Io(e))),
         }
     }
 }
-
-pub struct ChatCompletionRequest(String);
