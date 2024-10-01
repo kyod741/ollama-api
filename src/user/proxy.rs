d0ba1e1f0499d7f5e103320e9f94245040f63311
kyod741 <kyod741@staszic.waw.pl>
2024-10-01 11:16:33 +0200

    working on proxy, not tested yet. KS lesson

--- src/user/proxy.rs
+++ src/user/proxy.rs
@@ -1,1 +1,17 @@
+use crate::user::schemas::CompletionRequestBody;
+use std::env;
 
+pub async fn ollama_completion_request(
+    payload: &CompletionRequestBody,
+) -> Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let response = client
+        .post(env::var("OLLAMA_API_ADDRESS").expect("OLLAMA_API_ADDRESS must be set"))
+        .body(payload.0.clone())
+        .send()
+        .await?
+        .error_for_status()?;
+
+    let content = response.text().await?;
+    Ok(content)
+}
--- src/user/routers.rs
+++ src/user/routers.rs
@@ -1,11 +1,17 @@
+use crate::user::proxy::ollama_completion_request;
 use crate::user::schemas::{CompletionRequestBody, CompletionRequestHeaders, Wrapper};
 
-#[post("/completion", data = "<request>")]
-pub fn completion_request(
+#[post("/completion", data = "<payload>")]
+pub async fn completion_request(
     token: Wrapper<CompletionRequestHeaders>,
-    request: Wrapper<CompletionRequestBody>,
+    payload: Wrapper<CompletionRequestBody>,
 ) {
-    let unwrapped_token = token.0;
-    println!("{request:#?}");
-    println!("{unwrapped_token:#?}");
+    let unwrapped_payload = payload.0;
+    println!("{token:#?}");
+    println!("{unwrapped_payload:#?}");
+    let result = ollama_completion_request(&unwrapped_payload).await;
+    match result {
+        Ok(res) => println!("{res}"),
+        Err(e) => println!("{e:?}"),
+    }
 }
